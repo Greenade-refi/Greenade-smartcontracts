ffe78721f3e67ece639816d1258dfbdc0cee4f5c
Greenade-refi <teamgreenade@gmail.com>
2023-04-03 21:45:52 +0530

    config updates

--- .solcover.js
+++ .solcover.js
@@ -1,3 +1,4 @@
 module.exports = {
     skipFiles: ['test/fuzzing/KeepersCounterEchidnaTest.sol', 'test/LinkToken.sol', 'test/MockOracle.sol', 'test/MockV3Aggregator.sol', 'test/VRFCoordinatorV2Mock.sol'],
+    ignoreJs: true
 };
